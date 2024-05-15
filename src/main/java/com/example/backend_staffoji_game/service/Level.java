8dd273d8775ea5bae4d390cbbef970f8cc20a4ef
RobertMili <robertmilicevic3869@gmail.com>
2024-05-15 18:39:11 +0200

    add test and refactoring of code UserScoreService

---
+++ src/main/java/com/example/backend_staffoji_game/service/Level.java
@@ -0,0 +1,7 @@
+package com.example.backend_staffoji_game.service;
+
+public interface Level {
+    int LEVEL_ONE = 1;
+    int LEVEL_TWO = 2;
+    int LEVEL_THREE = 3;
+}
--- src/main/java/com/example/backend_staffoji_game/service/UserScoreService.java
+++ src/main/java/com/example/backend_staffoji_game/service/UserScoreService.java
@@ -1,14 +1,14 @@
 package com.example.backend_staffoji_game.service;
 
 import com.example.backend_staffoji_game.dto.UserScoreDTO;
+import com.example.backend_staffoji_game.exception.UserDoesNotExistsException;
 import com.example.backend_staffoji_game.model.UserScore;
 import com.example.backend_staffoji_game.repository.UserScoreRepository;
-import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserScoreService {
+public class UserScoreService implements Level {
 
     @Autowired
     private final UserScoreRepository userScoreRepository;
@@ -19,20 +19,36 @@ public class UserScoreService {
     }
 
     public UserScoreDTO updateUserScore(String userName, int level, int score) {
-        UserScore userScore = userScoreRepository.findByUserNameIs(userName)
-                .orElseThrow(() -> new EntityNotFoundException("No UserScore found with id: " + userName));
-        if (level == 1){
-            var updateScore = userScore.getLevel_one() + score;
-            userScore.setLevel_one(updateScore);
-        } else if (level == 2){
-            var updateScore = userScore.getLevel_two() + score;
-            userScore.setLevel_two(updateScore);
-        } else if (level == 3){
-            var updateScore = userScore.getLevel_three() + score;
-            userScore.setLevel_three(updateScore);
-        }
+        UserScore userScore = findUserScore(userName);
+        updateScore(level, score, userScore);
         userScoreRepository.save(userScore);
         return userScoreToUserScoreDTO(userScore);
+    }
+
+    private UserScore findUserScore(String userName) {
+        UserScore userScore = userScoreRepository.findByUserNameIs(userName)
+                .orElseThrow(() -> new UserDoesNotExistsException("No UserScore found with id: " + userName));
+        return userScore;
+    }
+
+    private static void updateScore(int level, int score, UserScore userScore) {
+        int updateScore;
+        switch (level) {
+            case LEVEL_ONE:
+                updateScore = userScore.getLevel_one() + score;
+                userScore.setLevel_one(updateScore);
+                break;
+            case LEVEL_TWO:
+                updateScore = userScore.getLevel_two() + score;
+                userScore.setLevel_two(updateScore);
+                break;
+            case LEVEL_THREE:
+                updateScore = userScore.getLevel_three() + score;
+                userScore.setLevel_three(updateScore);
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid level: " + level);
+        }
     }
 
     private UserScoreDTO userScoreToUserScoreDTO(UserScore userScore) {
