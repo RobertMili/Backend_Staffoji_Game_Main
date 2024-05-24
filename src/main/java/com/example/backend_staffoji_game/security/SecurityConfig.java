83ec38002240fc30cbe744791bbf554b626ce141
RobertMili <robertmilicevic3869@gmail.com>
2024-05-24 10:50:40 +0200

    refactoring oauth2

--- src/main/java/com/example/backend_staffoji_game/security/SecurityConfig.java
+++ src/main/java/com/example/backend_staffoji_game/security/SecurityConfig.java
@@ -6,14 +6,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -25,9 +20,9 @@ public class SecurityConfig {
                 .cors(Customizer.withDefaults())
                 .authorizeRequests(
                         authorizeRequests -> {
-                            authorizeRequests.requestMatchers("/").permitAll();
-                            authorizeRequests.requestMatchers("/swagger/**").authenticated();
-                            authorizeRequests.anyRequest().authenticated();
+                            authorizeRequests.requestMatchers(new AntPathRequestMatcher("/swagger-ui/**")).authenticated();
+                            authorizeRequests.anyRequest().permitAll();
+
                         })
                 .oauth2Login(oauth2Login -> oauth2Login
                         .defaultSuccessUrl("http://localhost:8083/swagger-ui/index.htm", true)
@@ -39,18 +34,5 @@ public class SecurityConfig {
     @Bean
     public AuthenticationSuccessHandler authenticationSuccessHandler() {
         return new CustomAuthenticationSuccessHandler();
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // Allow only this origin
-        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE")); // Allow these HTTP methods
-        configuration.setAllowCredentials(true);
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
     }
 }
